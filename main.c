08e65dd87231df58d8cc3ac9ed4960c3db153009
Adam Bławat <blawat.adam@gmail.com>
2014-04-07 12:11:28 +0200

    Dodałem detekcję krawędzi na pozostałych płaszczyznach.

--- main.c
+++ main.c
@@ -6,12 +6,18 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define SIZE_X 128
-#define SIZE_Y 128
+#define SIZE_X 1920
+#define SIZE_Y 1080
 
 int main()
 {
-    int i, j;
+    const char inFileName[]  = "/mnt/adam-2/dane/nature-2-1920-1080.raw";
+    const char outFileName[] = "/mnt/adam-2/dane/nature-2-1920-1080-edges.raw";
+    
+    int values[4];
+    int maxValue;
+    
+    int i, j, k;
     
     char **image  = malloc(sizeof(char *) * SIZE_Y);
     char **result = malloc(sizeof(char *) * SIZE_Y);
@@ -24,7 +30,7 @@ int main()
         memset(result[i], 0, SIZE_X * 3);
     }
     
-    int file = open("/mnt/adam-2/dane/image-1-128-128.raw", O_RDONLY);
+    int file = open(inFileName, O_RDONLY);
     
     for (i = 0; i < SIZE_Y; i++)
     {
@@ -33,15 +39,30 @@ int main()
     
     close(file);
     
-    for (i = 0; i < SIZE_Y; i++)
+    for (i = 1; i < SIZE_Y - 1; i++)
     {
-        for (j = 3; j < SIZE_X * 3; j++)
+        for (j = 3; j < SIZE_X * 3 - 3; j++)
         {
-            result[i][j] = (char)abs(image[i][j] - image[i][j - 3]);
+            values[0] = abs(image[i][j] - image[i][j - 3]);
+            values[1] = abs(image[i][j] - image[i - 1][j - 3]);
+            values[2] = abs(image[i][j] - image[i - 1][j]);
+            values[3] = abs(image[i][j] - image[i - 1][j + 3]);
+            
+            maxValue = values[0];
+            
+            for (k = 1; k < 4; k++)
+            {
+                if (values[k] > maxValue)
+                {
+                    maxValue = values[k];
+                }
+            }
+            
+            result[i][j] = (char)maxValue;
         }
     }
     
-    file = open("/mnt/adam-2/dane/image-1-128-128-edges.raw", O_WRONLY | O_CREAT, S_IRUSR);
+    file = open(outFileName, O_WRONLY | O_CREAT, S_IRUSR);
     
     for (i = 0; i < SIZE_Y; i++)
     {
